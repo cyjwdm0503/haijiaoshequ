73dc0543f1aa9b956812e334b446cb66be74427a
胖子王 <cyjwdm0503@foxmail.com>
2017-11-15 21:39:41 +0800

    补传少的文件

--- haijiaoshequ/haijiaoshequ/views.py
+++ haijiaoshequ/haijiaoshequ/views.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request,name='haijiaoshequ'):
-    return HttpResponse("Hello, world. You're at the "+ name +" index.")
+    template =  loader.get_template('index.html');
+    context = {'name':name}
+    return HttpResponse(template.render(context, request))
 # Create your views here.
