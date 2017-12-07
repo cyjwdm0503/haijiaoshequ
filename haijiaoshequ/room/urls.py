5bdf3933a91e923b4d1c0c0fde37dbb8cbc5db57
胖子王 <cyjwdm0503@foxmail.com>
2017-12-07 23:21:32 +0800

    异步刷新基础房间信息

--- haijiaoshequ/room/urls.py
+++ haijiaoshequ/room/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'base/(?P<room_id>[0-9]+)', views.main, {'name':'房间基础信息'}),
+    url(r'^', views.index, {'name': '海骄社区'})
 ]
--- haijiaoshequ/room/views.py
+++ haijiaoshequ/room/views.py
@@ -4,7 +4,23 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.http import JsonResponse
+from collections import OrderedDict
 
-def index(request):
+
+def index(request, name):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
 # Create your views here.
+
+
+
+def main(request, room_id, name):
+    context = OrderedDict({'id': room_id,
+               'base': {'info': '来自于十里彩云间，理工学子大家都懂的', 'money': "1230.0",'square':"09",'floor': '9', 'totalfloor': '21'},
+               'type': {"hezu": 1, "zhengzu": 0, "duwei": 1, "yangtai": 1, "piaochuang": 1, "liangren": 1,
+                        "kongtiao": 1, "nansheng": 0,
+                        "nvsheng": 1, "yi": 1, "er": 0, "san": 0, "si": 0, "liji": 1, "yizhou": 0, "yiyue":0 },
+               'img': {'src':'/static/phone/全部合租/房间8.jpg','alt':'房间8'}});
+    return JsonResponse(context, json_dumps_params={'sort_keys': True})
